e2daa0ced020632af19a0ff167062aefce93de1c
Lucas Carvalho De Jesus Oliveira <lucascarvalhoptc06@gmail.com>
2025-05-09 22:53:36 -0300

    Add files via upload

---
+++ C/LOOP/loop multiplos de 3.c
@@ -0,0 +1,28 @@
+/******************************************************************************
+Faça um programa que leia um número N (positivo) e, 
+usando um loop while, mostre todos os números de 1 até N que são múltiplos de 3.
+
+*******************************************************************************/
+# include<stdio.h>
+#include <math.h>
+
+int main (void)
+{
+    double n;
+    int contador = 1;
+    
+    printf("Qual é o valor ? ");
+    scanf("%lf", &n);
+    
+    if (n < 0) {
+        printf("Numero incorreto! o numero tem que se positvo.");
+    }else{
+        while(contador <= n){
+            if (contador % 3 == 0) {
+            printf("%d\n", contador);
+            }
+            contador++;
+        }
+    }
+    
+}
