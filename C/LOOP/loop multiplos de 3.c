cb547b65494ab898243841967fbda1cc5066a441
Lucas Carvalho De Jesus Oliveira <lucascarvalhoptc06@gmail.com>
2025-05-13 11:52:11 -0300

    Update loop multiplos de 3.c

--- C/LOOP/loop multiplos de 3.c
+++ C/LOOP/loop multiplos de 3.c
@@ -1,8 +1,9 @@
 /******************************************************************************
-Faça um programa que leia um número N (positivo) e, 
+Programa: multiplus de 3 em n.
+Autor: Lucas Carvalho
+Data: 09/05/2025
+Descrição: É um programa que lê um número N (positivo) e, 
 usando um loop while, mostre todos os números de 1 até N que são múltiplos de 3.
-09/05/2025
-
 *******************************************************************************/
 # include<stdio.h>
 #include <math.h>
